e7639ad20e2d9b0ad251a6ffa9a23425cd157b00
Etienne Lacazedieu <elacazedieu@atlassian.com>
2024-12-03 09:28:49 +0100

    D03 part 2

--- src/main/java/fr/ela/aoc2024/D03.java
+++ src/main/java/fr/ela/aoc2024/D03.java
@@ -1,5 +1,7 @@
 package fr.ela.aoc2024;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -7,6 +9,15 @@ public class D03 extends AoC {
 
     Pattern pattern = Pattern.compile("mul\\(([0-9]{1,3}),([0-9]{1,3})\\)");
 
+    public List<String> split(String line) {
+        List<String> dos = new ArrayList<>();
+        String[] elements = line.split("do\\(\\)");
+        for (String element : elements) {
+            int end =  element.indexOf("don't()");
+            dos.add(end == -1 ? element : element.substring(0, end));
+        }
+        return dos;
+    }
 
     public long mul(String line) {
         Matcher matcher = pattern.matcher(line);
@@ -20,7 +31,10 @@ public class D03 extends AoC {
     @Override
     public void run() {
         System.out.println("Test Result part 1 : "+mul(readFile(getTestInputPath())));
+        System.out.println("Test Result part 2 : "+split("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))").stream().mapToLong(this::mul).sum());
+
         System.out.println("Result part 1 : "+mul(readFile(getInputPath())));
+        System.out.println("Test Result part 2 : "+split(readFile(getInputPath())).stream().mapToLong(this::mul).sum());
 
     }
 }
