476c6321709cc97e21085dcc23080df83d05cb4a
Etienne Lacazedieu <elacazedieu@atlassian.com>
2024-12-03 09:59:39 +0100

    D03 part 2

--- src/main/java/fr/ela/aoc2024/D03.java
+++ src/main/java/fr/ela/aoc2024/D03.java
@@ -2,7 +2,6 @@ package fr.ela.aoc2024;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class D03 extends AoC {
@@ -13,28 +12,23 @@ public class D03 extends AoC {
         List<String> dos = new ArrayList<>();
         String[] elements = line.split("do\\(\\)");
         for (String element : elements) {
-            int end =  element.indexOf("don't()");
+            int end = element.indexOf("don't()");
             dos.add(end == -1 ? element : element.substring(0, end));
         }
         return dos;
     }
 
     public long mul(String line) {
-        Matcher matcher = pattern.matcher(line);
-        long result = 0;
-        while (matcher.find()) {
-            result += Long.parseLong(matcher.group(1)) * Long.parseLong(matcher.group(2));
-        }
-        return result;
+        return pattern.matcher(line).results().mapToLong(r -> Long.parseLong(r.group(1)) * Long.parseLong(r.group(2))).sum();
     }
 
     @Override
     public void run() {
-        System.out.println("Test Result part 1 : "+mul(readFile(getTestInputPath())));
-        System.out.println("Test Result part 2 : "+split("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))").stream().mapToLong(this::mul).sum());
+        System.out.println("Test Result part 1 : " + mul(readFile(getTestInputPath())));
+        System.out.println("Test Result part 2 : " + split("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))").stream().mapToLong(this::mul).sum());
 
-        System.out.println("Result part 1 : "+mul(readFile(getInputPath())));
-        System.out.println("Test Result part 2 : "+split(readFile(getInputPath())).stream().mapToLong(this::mul).sum());
+        System.out.println("Result part 1 : " + mul(readFile(getInputPath())));
+        System.out.println("Test Result part 2 : " + split(readFile(getInputPath())).stream().mapToLong(this::mul).sum());
 
     }
 }
