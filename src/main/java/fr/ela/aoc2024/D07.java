e072d5cfa5996ef927299df46c0cc724db7bad37
Etienne Lacazedieu <elacazedieu@atlassian.com>
2024-12-07 15:30:42 +0100

    D07 part 2

--- src/main/java/fr/ela/aoc2024/D07.java
+++ src/main/java/fr/ela/aoc2024/D07.java
@@ -2,9 +2,29 @@ package fr.ela.aoc2024;
 
 import java.nio.file.Path;
 import java.util.Arrays;
+import java.util.EnumSet;
 import java.util.List;
+import java.util.Set;
+import java.util.function.LongBinaryOperator;
 
 public class D07 extends AoC {
+
+    enum Operator implements LongBinaryOperator {
+        ADD(Long::sum),
+        MUL((l,r) -> l*r),
+        CONCAT((l,r) -> Long.parseLong(l+""+r));
+
+        private final LongBinaryOperator op;
+
+        Operator(LongBinaryOperator operator) {
+            this.op = operator;
+        }
+
+        @Override
+        public long applyAsLong(long left, long right) {
+            return op.applyAsLong(left, right);
+        }
+    }
 
     record Input(long result, long[] values) {
         static Input parse(String value) {
@@ -14,43 +34,41 @@ public class D07 extends AoC {
         }
     }
 
-    boolean isCorrect(Input input) {
-        return isCorrectFromIndex(input, 0, input.values[0]);
+    boolean isCorrect(Input input, Set<Operator> operators) {
+        return isCorrectFromIndex(input, 0, operators, input.values[0]);
     }
 
-    boolean isCorrectFromIndex(Input input, int index, long current) {
-        index++;
-        if (index == input.values.length) {
+    boolean isCorrectFromIndex(Input input, int index, Set<Operator> operators, long current) {
+        final int nextIndex = index + 1;
+        if (nextIndex == input.values.length) {
             return current == input.result;
         }
-        long next = input.values[index];
-        return isCorrectFromIndex(input, index, current + next) ||
-                isCorrectFromIndex(input, index, current * next);
+        long next = input.values[nextIndex];
+        return operators.stream().anyMatch(op -> isCorrectFromIndex(input, nextIndex, operators, op.applyAsLong(current, next)));
     }
 
     private static long[] toLongs(String[] strings) {
         return Arrays.stream(strings).mapToLong(Long::parseLong).toArray();
     }
 
-
-    public long solvePartOne(List<String> input) {
+    public long solve(List<String> input, Set<Operator> operators) {
         return input.stream()
                 .map(Input::parse)
-                .filter(this::isCorrect)
+                .filter(in -> isCorrect(in, operators))
                 .mapToLong(Input::result)
                 .sum();
     }
 
-    public void solve(Path inputPath, String s, long expected1) {
+    public void solve(Path inputPath, String s, long expected1, long expected2) {
         System.out.println("--- " + s + " ----");
         List<String> inputs = list(inputPath);
-        System.out.println("Part 1 ("+expected1+") : " + solvePartOne(inputs));
-
+        System.out.println("Part 1 ("+expected1+") : " + solve(inputs, EnumSet.of(Operator.ADD, Operator.MUL)));
+        System.out.println("Part 2 ("+expected2+") : " + solve(inputs, EnumSet.allOf(Operator.class)));
     }
 
     @Override
     public void run() {
-        solve(getTestInputPath(), "Test" ,3749);
-        solve(getInputPath(), "Real" ,1708857123053L);
+        solve(getTestInputPath(), "Test" ,3749,11387);
+        solve(getInputPath(), "Real" ,1708857123053L,0);
     }
 }
