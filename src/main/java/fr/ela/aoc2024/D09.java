aff1ce14ba8d4bebb749d18ad91395202d1971f5
Etienne Lacazedieu <elacazedieu@atlassian.com>
2024-12-10 18:38:42 +0100

    D09 part 2

--- src/main/java/fr/ela/aoc2024/D09.java
+++ src/main/java/fr/ela/aoc2024/D09.java
@@ -1,19 +1,66 @@
 package fr.ela.aoc2024;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
 import java.util.stream.IntStream;
+import fr.ela.aoc2024.utils.Pair;
 
 public class D09 extends AoC {
 
     static final char[] CHARS = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+    public class FileSystemElement {
+        int position;
+        int size;
+
+        public FileSystemElement(int position, int size) {
+            this.position = position;
+            this.size = size;
+        }
+        boolean isHole() {
+            return false;
+        }
+        int nextElementPosition() {
+            return position+size;
+        }
+
+        public String toString() {
+            return position+"."+size;
+        }
+    }
+    public class Hole extends FileSystemElement {
+        public Hole(int position, int size) {
+            super(position, size);
+        }
+
+        boolean isHole() {
+            return true;
+        }
+    }
+
+    public class File extends FileSystemElement {
+        final int id;
+
+        public File(int id, int position, int size) {
+            super(position, size);
+            this.id = id;
+        }
+        public String toString() {
+            return "["+id+"]"+position+"."+size;
+        }
+    }
 
     private class FileSystem {
+        final String filesystem;
         final int[] blocks;
-        final String blocksList;
+        final List<File> files = new LinkedList<>();
+        final List<Hole> holes = new LinkedList<>();
 
         public FileSystem(String s) {
+            this.filesystem = s;
             int size = s.chars().map(c -> c - '0').sum();
             blocks = new int[size];
-            this.blocksList = s;
             boolean file = true;
             int offset = 0;
             int id = 0;
@@ -27,7 +74,12 @@ public class D09 extends AoC {
                     }
                 }
                 if (file) {
+                    File f = new File(id, offset, length);
+                    files.add(f);
                     id++;
+                } else {
+                    Hole hole = new Hole(offset, length);
+                    holes.add(hole);
                 }
                 offset += length;
                 file = !file;
@@ -63,26 +115,66 @@ public class D09 extends AoC {
             }
         }
 
+        private Pair<Hole, Hole> surroundingHoles(File file) {
+            for (int i = 1; i <= holes.size(); i++) {
+                Hole before = holes.get(i - 1);
+                if (file.position == before.nextElementPosition()) {
+                    return new Pair<Hole,Hole>(before, i == holes.size() ? null : holes.get(i));
+                }
+            }
+
+            throw new IllegalStateException();
+        }
+
+        public void defrag2() {
+            for (int i = files.size() - 1; i > 1; i--) {
+                File file = files.get(i);
+                Iterator<Hole> holesIterator = holes.iterator();
+                var hole = holesIterator.next();
+                while (holesIterator.hasNext() && hole.size < file.size) {
+                    hole = holesIterator.next();
+                }
+                if ((hole.position >= file.position)) {
+                    continue;
+                }
+                if (hole.size >= file.size) {
+                    Pair<Hole, Hole> surrounding = surroundingHoles(file);
+                    surrounding.key().size += file.size;
+                    if (surrounding.value() != null) {
+                        surrounding.key().size = surrounding.key().size + file.size + surrounding.value().size;
+                        surrounding.value().size = 0;
+                    }
+                    for (int j = 0; j < file.size; j++) {
+                        blocks[hole.position + j] = file.id;
+                        blocks[file.position + j] = -1;
+                    }
+                    hole.position = hole.position + file.size;
+                    hole.size = hole.size - file.size;
+                }
+            }
+        }
 
         long checksum() {
-            defrag();
             return IntStream.range(0, blocks.length)
                     .filter(i -> blocks[i] > 0)
-                    .mapToLong(i -> (long)(i * blocks[i]))
+                    .mapToLong(i -> (long) (i * blocks[i]))
                     .sum();
         }
     }
-
 
 
     public void solve(String input, String s, long expected1, long expected2) {
         System.out.println("--- " + s + " ----");
         long time = System.currentTimeMillis();
         FileSystem fs = new FileSystem(input);
+        fs.defrag();
         long res = fs.checksum();
         time = System.currentTimeMillis() - time;
         System.out.println("Part 1 (" + expected1 + ") : " + res + " - " + time);
         time = System.currentTimeMillis();
+        fs = new FileSystem(input);
+        fs.defrag2();
+        res = fs.checksum();
         time = System.currentTimeMillis() - time;
         System.out.println("Part 2 (" + expected2 + ") : " + res + " - " + time);
     }
@@ -90,7 +182,7 @@ public class D09 extends AoC {
 
     @Override
     public void run() {
-        solve("2333133121414131402", "Test", 1928, 0L);
+        solve("23331331214141314023", "Test", 1928, 2858L);
         solve(readFile(getInputPath()), "Real", 6435922584968L, 0L);
     }
 }
